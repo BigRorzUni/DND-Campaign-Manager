7d1a04b77eda17203150f501847c70880975680a
RoryEdmonds <123957532+BigRorzUni@users.noreply.github.com>
2026-03-08 16:33:52 +0000

    Added simple UI

--- app/ui/routes.py
+++ app/ui/routes.py
@@ -13,3 +13,31 @@ def home(request: Request):
         name="home.html",
         context={}
     )
+
+@router.get("/campaigns/{campaign_id}/view", response_class=HTMLResponse)
+def campaign_view(request: Request, campaign_id: int):
+    return templates.TemplateResponse(
+        request=request,
+        name="campaign.html",
+        context={"campaign_id": campaign_id}
+    )
+
+@router.get("/sessions/{session_id}/view", response_class=HTMLResponse)
+def session_view(request: Request, session_id: int):
+    return templates.TemplateResponse(
+        "session.html",
+        {
+            "request": request,
+            "session_id": session_id
+        }
+    )
+
+@router.get("/encounters/{encounter_id}/view", response_class=HTMLResponse)
+def encounter_view(request: Request, encounter_id: int):
+    return templates.TemplateResponse(
+        "encounter.html",
+        {
+            "request": request,
+            "encounter_id": encounter_id
+        }
+    )
